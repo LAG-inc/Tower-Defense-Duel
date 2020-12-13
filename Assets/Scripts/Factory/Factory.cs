840fc73d065a850f62c99c4cfb58f32fc4b53d23
Daniel Luque <danielluque14@gmail.com>
2020-12-13 01:31:47 +0100

    Implementación base del sistema de fabricas

---
+++ Assets/Scripts/Factory/Factory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+    private GameObject _unitToGenerate;
+
+    public void PrepareUnitGeneration(GameObject unit)
+    {
+        Debug.Log($"Instanciar: {unit.name}");
+        _unitToGenerate = unit;
+        FactoryTracker.PlaceUnit();
+    }
+
+    public void GenerateUnit(Vector3 location)
+    {
+        var position = new Vector2(location.x, location.y);
+        Instantiate(_unitToGenerate, position, Quaternion.identity);
+    }
+}
